899c02367cf2faf14f0ac057b91dbf24bc075aea
aronresni <aronresni@gmail.com>
2022-12-01 23:00:28 -0300

    third commit

--- Js/main.js
+++ Js/main.js
@@ -45,80 +45,32 @@ function mostrarHabilidades(array) {
 }
 
 //Funcion para agregar al carrito
-function agregarAlCarrito(habilidad) {
-  //Primer paso
-  habilidadesEnCarro.push(habilidad)
-  console.log(habilidadesEnCarro)
-  localStorage.setItem("carrito", JSON.stringify(habilidadesEnCarro))
 
-  //Usamos Sweet Alert
-  Swal.fire({
-    //position: "top-end" ,
-    tittle: "Ha agregado una habilidad",
-    icon: "success",
-    confirmButtonText: "Entendido",
-    confirmButtonColor: "green",
-    timer: 2500,
-    text: `La habilidad ${habilidad.lenguaje} con la duracion de ${habilidad.horas} horas ha sido agregada`,
 
-  })
-
-}
 //Function para imprimir el modal del carrito  
-function cargarProductosCarrito(array) {
-  modalBodyHabilidades.innerHTML = ""
-  array.forEach((habilidadesEnCarro) => {
-    modalBodyHabilidades.innerHTML += `
-    <div class="card border-primary mb-3" id ="habilidadesEnCarro${habilidadesEnCarro.id}" style="max-width: 540px;">
-        <img class="card-img-top" height="300px" src="assets/${habilidadesEnCarro.imagen}" alt="${habilidadesEnCarro.lenguaje}">
-        <div class="card-body">
-                <h4 class="card-title">${habilidadesEnCarro.titulo}</h4>
-            
-                <p class="card-text">$${habilidadesEnCarro.precio}</p> 
-                <button class= "btn btn-danger" id="botonEliminar${habilidadesEnCarro.id}"><i class="fas fa-trash-alt"></i></button>
-        </div>    
-    </div>
-`
-  })
-  array.forEach((habilidadesEnCarro, indice) => {
+
+
     //Capturo elemento dom sin guardarlo en la variable
-    document.getElementById(`botonEliminar${habilidadesEnCarro.id}`).addEventListener("click", () => {
+
 
       //Eliminar el dom
-      let cardHabilidad = document.getElementById(`habiliadadesEnCarro ${habilidadesEnCarro.id}`)
-      cardHabilidad.remove()
+
       //eliminar array de comprar
-      habilidadesEnCarro.splice(indice, 1)
-      console.log(habilidadesEnCarro)
+
       //Eliminar Storage
-      localStorage.setItem(`carrito`, JSON.stringify(habilidadesEnCarro))
+
       //Vuelvo a calcular el total
-      compraTotal(array)
-    })
-  })
-  compraTotal(array)
-}
+
 
 
 
 //function calcular total
-function compraTotal(array) {
-  let acumulador = 0
-  acumuladormulador = array.reduce((acc, habilidadesEnCarro) => acc + habilidadesEnCarro.precio, 0)
-  console.log(acumulador)
-  acumulador == 0 ? divCompra.inner = `No hay productos en el carrito` : divCompra.innerHTML`El total de su carrito es ${acumulador}`
-}
+
 //function tnuscador ternario
-function buscarInfo(buscado, array) {
-  let busqueda = array.filter(
-    (habilidad) => habilidad.id.toLowerCase().includes(buscado.toLowerCase()) || habilidad.lenguaje.toLowerCase().includes(buscado.toLowerCase())
-  )
+
 
   //con ternario
-  busqueda.length == 0 ?
-    (coincidencia.innerHTML = `<h3 class="text-success m-2">No hay coincidencias con su búsqueda.. a continuación tiene todo nuestro catálogo disponible</h3>`, mostrarCatalogo(array))
-    : (coincidencia.innerHTML = "", mostrarHabilidades(busqueda))
-}
+
 
 //Evento de agregar al carro
 botonCarrito.addEventListener("click", () => {
